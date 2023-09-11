84b631f3585bba95765ba9e975cca0413d0b417a
Harini-17-sarav <harinisarav17@gmail.com>
2023-09-11 15:56:54 +0530

    Harini first commit

---
+++ src/hello/world.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("TRYING TO EXPLORE TO GIT");
+
+		System.out.println("We will also explore local repo and remote repo");
+
+		System.out.println("We will also explore gitbash and github");
+	}
+
+}
